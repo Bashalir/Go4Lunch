20db4ed632e2bf1164aefefedda5c95b44bf11c2
Bashalir <bachir@sahali.fr>
2018-11-26 23:11:50 +0100

    Add Logo and Modify activity_main.xml

--- app/src/main/java/com/bashalir/go4lunch/MainActivity.java
+++ app/src/main/java/com/bashalir/go4lunch/MainActivity.java
@@ -2,12 +2,27 @@ package com.bashalir.go4lunch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+@BindView(R.id.logo_iv)
+    ImageView logo;
+@BindView(R.id.slogan_tv)
+    TextView slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        slogan.setText(R.string.slogan);
     }
+
+
 }
