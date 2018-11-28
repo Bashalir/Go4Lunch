003ea492db00098c109527f65eb6a6181b8469f1
Bashalir <bachir@sahali.fr>
2018-11-28 16:05:11 +0100

    Add Facebook Authentication

--- app/src/main/java/com/bashalir/go4lunch/MainActivity.java
+++ app/src/main/java/com/bashalir/go4lunch/MainActivity.java
@@ -10,6 +10,9 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 import java.util.Arrays;
 import java.util.List;
