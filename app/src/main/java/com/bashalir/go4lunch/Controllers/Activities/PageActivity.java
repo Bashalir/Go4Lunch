1a198a6a3a65083fe5a5c376bea825166241fc44
Bashalir <bachir@sahali.fr>
2018-12-03 15:03:07 +0100

    Fix search button in toolbar

--- app/src/main/java/com/bashalir/go4lunch/Controllers/Activities/PageActivity.java
+++ app/src/main/java/com/bashalir/go4lunch/Controllers/Activities/PageActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import butterknife.BindView;
@@ -44,6 +45,28 @@ public class PageActivity extends AppCompatActivity {
         //2 - Inflate the menu and add it to the Toolbar
         getMenuInflater().inflate(R.menu.menu_activity_page, menu);
         return true;
+    }
+
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //3 - Handle actions on menu items
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_activity_main_search:
+
+                Toast.makeText(this, "Recherche indisponible, demandez plutôt l'avis de Google, c'est mieux et plus rapide.", Toast.LENGTH_LONG).show();
+
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
 
     private void configureToolbar() {
